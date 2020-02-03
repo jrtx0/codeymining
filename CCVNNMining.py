d0c1392a28a29d9763a950621007526521a8f70d
Ren Ji <1511198943@qq.com>
2020-02-03 14:31:46 +0800

    Update CCVNNMining.py
    实现登录功能

--- CCVNNMining.py
+++ CCVNNMining.py
@@ -2,8 +2,20 @@
 
 import requests
 
-email = '1511198943@qq.com'
+# 根据你的用户名和密码修改相应位置的值
+username = 'JRTx'
 password = '19960321'
-loginturl = ''
+loginurl = 'http://bbs.codedy.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
 
+# 这行代码是用来维持cookie的，你后续操作都不用担心cookie，他会自动带上相应的cookie
 s = requests.Session()
+# 带表单的参数
+loginparams = {'username':username, 'password':password, 'quickforward':'yes'}
+# post数据实现登录
+r = s.post(loginurl, data = loginparams)
+# 验证是否登录成功，抓取首页看看内容
+r = s.get(loginurl)
+print(r.text)
+withdrawUrl = 'http://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=d4c771c8&t=0.7761234471790155'
+r = s.post(withdrawUrl)
+print("领取收益成功")

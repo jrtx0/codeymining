00154e0bd4fabb4927601b7bd032fcb1a049d4a3
Ren Ji <1511198943@qq.com>
2020-07-07 22:04:36 +0800

    finish all function

--- CCVNNMining.py
+++ CCVNNMining.py
@@ -3,12 +3,15 @@
 import requests
 import re
 import time
+import random
 
 
 # 根据你的用户名和密码修改相应位置的值
 username = 'JRTx'
 password = '19960321'
-loginurl = 'http://bbs.codedy.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
+loginurl = 'https://bbs.codedy.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LAX1v&inajax=1'
+minerurl = 'https://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner'
+checkurl = 'https://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=d9d2a125&t=0.33848210890214014'
 
 # 这行代码是用来维持cookie的，你后续操作都不用担心cookie，他会自动带上相应的cookie
 s = requests.Session()
@@ -16,15 +19,12 @@ s = requests.Session()
 loginparams = {'username':username, 'password':password}
 # post数据实现登录
 s.post(loginurl, data = loginparams)
-# 验证是否登录成功，抓取首页看看内容
-r = s.get(loginurl)
-print(r.text)
-time.sleep(1)
-# withdrawUrl = 'http://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=d4c771c8&t=0.11143162409816232'
 
-# withdrawUrl = 'http://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=c30ada4a&t=0.4462692100109882'
+formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', r.text).group(1)
+t = random.random()
+checkurl = 'https://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=' + formhash + '&t=' + str(t)
+# post数据实现收取金币
+s.post(checkurl)
+print(checkurl)
 
-# withdrawUrl = 'http://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=c30ada4a&t=0.3032894840180431'
-withdrawUrl = 'http://bbs.codedy.com/plugin.php''
-# r = s.post(withdrawUrl)
-# print(r.content)
+

27afaeae83898bce30246cb5f13e3c781867589c
吉人 <jiren@bdstar.com>
2020-07-23 14:11:59 +0800

    refactor code

--- CodeyMining.py
+++ CodeyMining.py
@@ -5,38 +5,44 @@ import re
 import time
 import random
 
-
 # 根据你的用户名和密码修改相应位置的值
 username = '####'
 password = '####'
 
-def get_formhash():
-    mainurl = 'https://bbs.codedy.com/forum.php'
-    content = s.get(mainurl) 
+login_url = 'https://bbs.codedy.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LAX1v&inajax=1'
+
+def extract_formhash():
+    home_page = 'https://bbs.codedy.com/forum.php'
+    content = session.get(home_page)
     formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', content.text).group(1)
     return formhash
 
-def get_checkurl():
+def join_checkurl():
     t = random.random()
-    checkurl = 'https://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=' + get_formhash() + '&t=' + str(t)
+    checkurl = 'https://bbs.codedy.com/plugin.php?id=hux_miner:hux_miner&ac=draw&formhash=' +\
+               extract_formhash() + '&t=' + str(t)
     return checkurl
 
-def get_signurl():
+def join_signurl():
     t = random.random()
-    signurl = 'https://bbs.codedy.com/plugin.php?id=k_misign:sign&operation=qiandao&formhash=' + get_formhash() + '&format=empty&inajax=1&ajaxtarget=JD_sign'
+    signurl = 'https://bbs.codedy.com/plugin.php?id=k_misign:sign&operation=qiandao&formhash=' +\
+              extract_formhash() + '&format=empty&inajax=1&ajaxtarget=JD_sign'
     return signurl
 
-if __name__ == '__main__':
-    loginurl = 'https://bbs.codedy.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LAX1v&inajax=1'
+def main():
     # 这行代码是用来维持cookie的，你后续操作都不用担心cookie，他会自动带上相应的cookie
-    s = requests.Session()
+    global session
+    session = requests.Session()
     # 带表单的参数
-    loginparams = {'username':username, 'password':password}
-    s.post(loginurl, data = loginparams)
+    login_params = {'username': username, 'password': password}
+    session.post(login_url, login_params)
     # 每日签到
-    s.post(get_signurl())
+    session.post(join_signurl())
     # 收取挖矿收益
-    s.post(get_checkurl())
+    session.post(join_checkurl())
+
+if __name__ == '__main__':
+    main()
 
 
 
